3e773d9fe39e8ca585efa842ce26262781a547c4
Matt Snow <mattsnow@gmail.com>
2023-01-25 22:20:10 -0700

    Update entrypoint.sh
    added maxrate and bufsize per youtube recs.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-
-YTS_KEY=
 YTS_URL=rtmp://a.rtmp.youtube.com/live2
-
 
 libcamera-vid --inline --nopreview -t 0 --width 1920 --height 1080 --framerate 30 --codec h264 -o - | \
   ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -thread_queue_size 1024 \
     -use_wallclock_as_timestamps 1 -i pipe:0 -c:v copy -c:a aac -preset fast -strict experimental \
-    -f flv ${YTS_URL}/${YTS_KEY}
+    -b:v 4500k -maxrate 4500k -bufsize 4500k -f flv ${YTS_URL}/${YTS_KEY}
